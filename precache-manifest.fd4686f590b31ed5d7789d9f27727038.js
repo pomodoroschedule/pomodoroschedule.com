a0421520cf74db5f4c06802ed21f015c3d0cbe14
1337code <am1337@gmail.com>
2020-05-08 16:15:16 +0100

    Simple 25 minute clock

---
+++ precache-manifest.fd4686f590b31ed5d7789d9f27727038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0bac8413b3511c419ca8aa9d8cf251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30aad9f7e6efd520407e",
+    "url": "/static/css/main.edd73c4a.chunk.css"
+  },
+  {
+    "revision": "b8a9f9da7ee68933d0cc",
+    "url": "/static/js/2.dd8efee9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd8efee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30aad9f7e6efd520407e",
+    "url": "/static/js/main.9be4c3fd.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
